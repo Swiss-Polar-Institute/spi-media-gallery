1b07ccf5fdf8eb2f7be886edeee03f790948cf12
Carles Pina i Estany <carles@pina.cat>
2019-06-19 10:33:32 +0200

    Adds tool to reset the database.

---
+++ SpiMediaGallery/reset-database.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "drop database spi_media_gallery; create database spi_media_gallery;" | python3 manage.py dbshell
+
+rm -rf main/migrations/
+
+echo "Make migrations"
+python3 manage.py makemigrations main
+
+echo "Migrate:"
+python3 manage.py migrate
+
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
