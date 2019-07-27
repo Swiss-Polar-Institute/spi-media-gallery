b934c4ec053dd8def89be0dfb95f8547755f2533
Carles Pina i Estany <carles@pina.cat>
2019-07-27 08:36:14 +0200

    Allows height to be null.

--- SpiMediaGallery/main/models.py
+++ SpiMediaGallery/main/models.py
@@ -108,7 +108,7 @@ class MediumResized(models.Model):
 
     size_label = models.CharField(max_length=1, choices=SIZES_OF_PHOTOS)
 
-    height = models.IntegerField()
+    height = models.IntegerField(null=True, blank=True)
     width = models.IntegerField(null=True, blank=True)
     medium = models.ForeignKey(Medium, on_delete=models.PROTECT)
 
