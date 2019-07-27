a679e132cc9c1061625a07585722e0e9cdae8003
Carles Pina i Estany <carles@pina.cat>
2019-07-27 08:50:22 +0200

    Avoids having null values.
    This was due to a problem encoding files.

--- SpiMediaGallery/main/models.py
+++ SpiMediaGallery/main/models.py
@@ -108,8 +108,8 @@ class MediumResized(models.Model):
 
     size_label = models.CharField(max_length=1, choices=SIZES_OF_PHOTOS)
 
-    height = models.IntegerField(null=True, blank=True)
-    width = models.IntegerField(null=True, blank=True)
+    height = models.IntegerField()
+    width = models.IntegerField()
     medium = models.ForeignKey(Medium, on_delete=models.PROTECT)
 
     @staticmethod
