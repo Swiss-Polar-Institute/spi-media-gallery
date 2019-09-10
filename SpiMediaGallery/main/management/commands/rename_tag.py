f1b7eca0a47a6e9d5ddb7c22be71af808dc5f60a
Carles Pina i Estany <carles@pina.cat>
2019-09-10 10:02:35 +0200

    Adds unique key for tag names.
    Note that rename_tag.py command is not done yet.

---
+++ SpiMediaGallery/main/management/commands/rename_tag.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+
+from ...models import Medium, TagName
+
+import re
+
+class Command(BaseCommand):
+    help = 'Rename tags'
+
+    def add_arguments(self, parser):
+        parser.add_argument('old_tag', type=str,
+                            help='Tag to be renamed.')
+        parser.add_argument('new_tag', type=str,
+                            help='New tag name')
+
+    def handle(self, *args, **options):
+        old_tag = options['old_tag']
+        new_tag = options['new_tag']
+
+        for tag_name in TagName.objects.filter(name__startswith=old_tag):
+            tag_name.name = re.sub('^' + old_tag, new_tag, tag_name.name)
+            tag_name.save()
+
+        TagName.objects.filter(name=old_tag).update(name=new_tag)
--- SpiMediaGallery/main/models.py
+++ SpiMediaGallery/main/models.py
@@ -46,7 +46,7 @@ class Copyright(models.Model):
 class TagName(models.Model):
     objects = models.Manager()  # Helps Pycharm CE auto-completion
 
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
