74038309e872a38af131a3824f22a49ae32794cb
Jen Thomas <jenny_t152@yahoo.co.uk>
2019-09-10 16:15:50 +0100

    Created class to rename a tag from old to new. Renames if the tag does not exist. Aborts if it already exists.

--- SpiMediaGallery/main/management/commands/rename_tag.py
+++ SpiMediaGallery/main/management/commands/rename_tag.py
@@ -1,6 +1,7 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
 
-from ...models import Medium, TagName
+from ...models import Medium, TagName, Tag
 
 import re
 
@@ -18,8 +19,37 @@ class Command(BaseCommand):
         old_tag = options['old_tag']
         new_tag = options['new_tag']
 
-        for tag_name in TagName.objects.filter(name__startswith=old_tag):
-            tag_name.name = re.sub('^' + old_tag, new_tag, tag_name.name)
-            tag_name.save()
+        print("Old:", old_tag, "New:", new_tag)
 
-        TagName.objects.filter(name=old_tag).update(name=new_tag)
+        modifier = ModifyTag()
+        modifier.rename(old_tag, new_tag)
+        #
+        # for tag_name in TagName.objects.filter(name__startswith=old_tag):
+        #     tag_name.name = re.sub('^' + old_tag, new_tag, tag_name.name)
+        #     tag_name.save()
+        #
+        # TagName.objects.filter(name=old_tag).update(name=new_tag)
+
+class ModifyTag:
+    def __init__(self):
+        pass
+
+
+    def tag_name_is_in_database(self, tag):
+        try:
+            TagName.objects.get(name=tag)
+            return True
+        except ObjectDoesNotExist:
+            return False
+
+
+    def rename(self, old, new):
+
+        if self.tag_name_is_in_database(new):
+            '''Check to see if new tag exists. If it exists, abort.'''
+            print('Tag already exists, aborting.')
+        else:
+            '''If it does not exist, create the tag and tag all the media that are already tagged with the old tag, with the new tag.'''
+            tag_name = TagName.objects.get(name=old)
+            tag_name.name = new
+            tag_name.save()
