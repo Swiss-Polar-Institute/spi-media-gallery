237ae33ca1c22aa6d28c19139ed624fa391c5622
Carles Pina i Estany <carles@pina.cat>
2019-09-10 09:56:07 +0200

    Prints photographers that are not in the database.

--- SpiMediaGallery/main/management/commands/update_photographers.py
+++ SpiMediaGallery/main/management/commands/update_photographers.py
@@ -24,30 +24,48 @@ class PhotographerUpdater(object):
         progress_report = ProgressReport(photos_without_photographer_count,
                                          extra_information='Updating photographer of photos')
 
+        invalid_photographers = set()
+
         for photo in photos_without_photographer:
             progress_report.increment_and_print_if_needed()
 
             photographer_str = self._get_photographer_string(photo.tags.all())
 
             if photographer_str is None:
-                print('Photo without photographer tag:', photo.object_storage_key)
+                print('Photo without photographer tag:', photo.file.object_storage_key)
                 continue
 
             photographer = self._get_photographer(photographer_str)
 
-            photo.photographer = photographer
-            photo.save()
+            if photographer is None:
+                invalid_photographers.add(photographer_str)
+            else:
+                photo.photographer = photographer
+                photo.save()
+
+        print('Invalid photographers: {}'.format(invalid_photographers))
 
     @staticmethod
     def _get_photographer(string):
+        if string.count('_') != 1:
+            print('Invalid photographer tag format: {}'.format(string))
+            return None
+
         first_name, last_name = string.split('_')
         photographer = Photographer.objects.filter(first_name=first_name).filter(last_name=last_name)
-        assert len(photographer) == 1
-        return photographer[0]
+
+        if len(photographer) == 1:
+            return photographer[0]
+        elif len(photographer) > 1:
+            print('More than one photographer in the DB for {}'.format(string))
+        elif len(photographer) == 0:
+            print('Photographer cannot be found in the database: {}'.format(string))
+
+        return None
 
     @staticmethod
     def _get_photographer_string(tags):
         prefix = 'photographer/'
         for tag in tags:
-            if tag.tag.startswith(prefix):
-                return tag.tag[len(prefix):]
+            if tag.name.name.startswith(prefix):
+                return tag.name.name[len(prefix):]
