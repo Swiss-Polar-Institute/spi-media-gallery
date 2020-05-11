e2409099c21721c41e0eaf91f6c4459689ce069f
Carles Pina i Estany <carles@pina.cat>
2020-05-11 16:52:14 +0200

    Fixes backslash that shouldn't be there

--- SpiMediaGallery/SpiMediaGallery/urls.py
+++ SpiMediaGallery/SpiMediaGallery/urls.py
@@ -21,4 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
