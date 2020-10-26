0e9d1c63bcb21ae9bdbbc7a59e16c9cbb6b40a17
Carles Pina i Estany <carles@pina.cat>
2020-10-26 16:28:11 +0000

    Add environment variables for running the unit tests

--- tools/environment-variables-for-test.sh
+++ tools/environment-variables-for-test.sh
@@ -1,2 +1,15 @@
 export PROJECT_APPLICATION_API_KEY=a_string_shared_with_project_application
 export PROJECT_APPLICATION_BASE_URL=http://localhost:8010
+
+export FROM_EMAIL=notification@example.com
+export EMAIL_HOST=smtp.example.com
+export EMAIL_HOST_USER=john@example.com
+export EMAIL_HOST_PASSWORD=this_is_not_the_password
+export EMAIL_SUBJECT_PREFIX=[ProjectsApplicationUnitTest]
+export ADMIN_1=Name,name@example.com
+export DEBUG=1
+export ALLOWED_HOST_1=127.0.0.1
+
+export SECRET_KEY="NOT-USED-ONLY-TESTS-zz!c&6qne#+x8(03_q30wz%z93)0&h9&(%)=5+e+6bn-87ok^g"
+export SECURE_SSL_REDIRECT=0
+
