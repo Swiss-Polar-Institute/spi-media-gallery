5f858b2a60f388a7ce46b1ebe508703b5050b092
Carles Pina i Estany <carles@pina.cat>
2020-11-02 15:54:56 +0000

    Create new file for the unit test

---
+++ SpiMediaGallery/main/tests/test_delete_medium.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+
+class DeleteMedium(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_delete_medium(self):
+        pass
