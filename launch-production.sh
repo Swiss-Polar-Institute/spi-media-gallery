36150dae4d2980b8037c82751e176b0360108a3b
Carles Pina i Estany <carles@pina.cat>
2021-02-12 11:33:48 +0000

    Add database support in docker-compose

--- launch-production.sh
+++ launch-production.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo "You might need to do: docker build -t spi-media-gallery-docker:0.0.1 ."
-
-echo "docker-compose is not re-building this image"
+echo "You might need to do: ./docker-build.sh"
 
 docker-compose -f docker-compose.yml down
 docker-compose -f docker-compose.yml up --build -d
