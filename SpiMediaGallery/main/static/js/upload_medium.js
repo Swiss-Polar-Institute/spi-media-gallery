5c3f0937ee40fac59ff94fec2a9a6a6e2d485f25
Marc-Antoine Cloux <marc-antoine@darwindigital.com>
2022-09-28 16:26:54 +0200

    add: support for tags

--- SpiMediaGallery/main/static/js/upload_medium.js
+++ SpiMediaGallery/main/static/js/upload_medium.js
@@ -1,20 +1,40 @@
-document.getElementById('medium_form').addEventListener('submit', function(event) {
+function add() {
+    var element = document.createElement("input");
+
+    //Assign different attributes to the element.
+    element.setAttribute("type", "text");
+    element.setAttribute("name", "tag_input");
+
+    var tags = document.getElementById("tags");
+
+    //Append the element in page (in span).
+    tags.appendChild(element);
+}
+
+document.getElementById("medium_form").addEventListener("submit", function(event) {
     event.preventDefault();
-    let file_input = document.getElementById('file_input');
-    let medium_type_input = document.getElementById('medium_type_input').value;
+    let file_input = document.getElementById("file_input");
+    let medium_type_input = document.getElementById("medium_type_input").value;
+    let tags = document.getElementById("tags").childNodes;
+    console.log(tags);
+    console.log(tags.length);
 
     let data = new FormData();
-    data.append('file', file_input.files[0]);
-    data.append('medium_type', medium_type_input);
+    data.append("file", file_input.files[0]);
+    data.append("medium_type", medium_type_input);
 
-    fetch('/api/v1/medium/', {
-        method: 'POST',
+    for(var i = 0; i < tags.length; i++) {
+        console.log(tags[i].value);
+        data.append("tag" + i, tags[i].value);
+    }
+    fetch("/api/v1/medium/", {
+        method: "POST",
         body: data
     }).then(function(response){
         return response.json();
     }).then(function(data) {
         console.log(data);
     }).catch(function(error) {
-        console.error('Error:', error);
+        console.error("Error:", error);
     });
 });
