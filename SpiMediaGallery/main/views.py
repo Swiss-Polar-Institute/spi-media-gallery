64663149cd356d15a21b333b64c2a26c2684722e
Neeraj <neeraj@darwindigital.com>
2024-05-15 17:43:46 +0530

    SPWS01-29

--- SpiMediaGallery/main/views.py
+++ SpiMediaGallery/main/views.py
@@ -1078,8 +1078,8 @@ class MediumView(TemplateView):
                     qs = qs.filter(datetime_taken__year=order_by_year)
             if "order_by_dpi" in request.COOKIES.keys():
                 order_by_dpi = request.COOKIES.get("order_by_dpi", "default")
-                if order_by_dpi == "dpi":
-                    qs = MediumForView.objects.annotate(
+                if order_by_dpi in ["dpi", "-dpi"]:
+                    qs = qs.annotate(
                         dpi=ExpressionWrapper(
                             Func(
                                 F('width') * F('width') + F('height') * F('height'),
@@ -1093,23 +1093,11 @@ class MediumView(TemplateView):
                             ),
                             output_field=FloatField()
                         )
-                    ).order_by('dpi')
-                else:
-                    qs = MediumForView.objects.annotate(
-                        dpi=ExpressionWrapper(
-                            Func(
-                                F('width') * F('width') + F('height') * F('height'),
-                                function='SQRT'
-                            ) / ExpressionWrapper(
-                                Func(
-                                    Value(1.0) * Value(1.0),
-                                    function='SQRT'
-                                ),
-                                output_field=FloatField()
-                            ),
-                            output_field=FloatField()
-                        )
-                    ).order_by('-dpi')
+                    )
+                    if order_by_dpi == "dpi":
+                        qs = qs.order_by('dpi')
+                    else:
+                        qs = qs.order_by('-dpi')
 
             if "preselect_status" in request.COOKIES.keys():
                 preselect_status = request.COOKIES.get("preselect_status", "default")
